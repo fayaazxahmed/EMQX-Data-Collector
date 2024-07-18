3484cd4c97bcf561ae405e785039ff732e242758
Fayaaz <fayaaz.ahmed.20@gmail.com>
2024-07-18 15:19:35 -0400

    Update publisher topic

--- main.go
+++ main.go
@@ -55,7 +55,7 @@ func publish(client mqtt.Client) {
 	num := 10
 	for i := 0; i < num; i++ {
 		text := fmt.Sprintf("Message %d", i)
-		token := client.Publish("topic/test", 0, false, text)
+		token := client.Publish("root/faux/config", 0, false, text)
 		token.Wait()
 		time.Sleep(time.Second)
 	}
